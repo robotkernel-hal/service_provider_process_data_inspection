989d3928f0811faa6e2e8c3bef2533e3ae7658c6
Johannes Nix <johannes.nix@dlr.de>
2022-10-17 18:14:29 +0200

    convert for GTK3

---
+++ bindings/python/rk_gtk3_gui_plugin/service_provider_process_data_inspection/process_data_inspection_wrapper.py
@@ -0,0 +1,34 @@
+'''
+(C) Robert Burger <robert.burger@dlr.de>
+
+This file is part of Robotkernel-GUI.
+
+Robotkernel-GUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Robotkernel-GUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Robotkernel-GUI.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import time, traceback
+import helpers
+
+class process_data_inspection_device(helpers.svc_wrapper):
+    def __init__(self, clnt, prefix):
+        helpers.svc_wrapper.__init__(self, clnt, prefix)
+        self.in_data = []
+        self.out_data = []
+
+    def update(self):
+        self.svc_in.call()
+        self.in_data = self.svc_in.resp.data
+
+        self.svc_out.call()
+        self.out_data = self.svc_out.resp.data
